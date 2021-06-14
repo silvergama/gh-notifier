563e4424385ce1f54cbdaa611fb4956ca0a929c1
Silver Gama <silver.mdg@gmail.com>
2021-06-14 19:13:35 -0300

    Refactoring notification

--- main.go
+++ main.go
@@ -4,48 +4,59 @@ import (
 	"context"
 	"log"
 	"os"
+	"os/exec"
+	"strings"
 
-	"github.com/gen2brain/beeep"
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 )
 
 type Notification struct {
-	Title string
-	Type  string
-	URL   string
+	Title     string
+	Subtitle  string
+	Message   string
+	URL       string
+	AvatarURL string
 }
 
 func NewNotification(ghn *github.Notification) *Notification {
+	msg := strings.ReplaceAll(ghn.GetSubject().GetTitle(), "[", `\[`)
 	return &Notification{
-		Title: ghn.GetRepository().GetName(),
-		Type:  ghn.GetSubject().GetType(),
-		URL:   ghn.GetSubject().GetURL(),
+		Title:     ghn.GetRepository().GetName(),
+		Subtitle:  ghn.GetSubject().GetType(),
+		Message:   msg,
+		URL:       "https://github.com/notifications",
+		AvatarURL: ghn.GetRepository().GetOwner().GetAvatarURL(),
 	}
 }
 
-func (n Notification) Notify() {
-	err := beeep.Notify(n.Title, n.Type, "")
+// Notify sends desktop notification.
+func (n Notification) Notify() error {
+	tn, err := exec.LookPath("terminal-notifier")
 	if err != nil {
-		log.Fatalf("Error %v", err)
+		return err
 	}
+	cmd := exec.Command(tn, "-title", n.Title, "-subtitle", n.Subtitle, "-message", n.Message, "-contentImage", n.AvatarURL, "-open", n.URL)
+
+	return cmd.Run()
 }
 
 func main() {
 	token := os.Getenv("GH_TOKEN")
 	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: token},
-	)
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
 	tc := oauth2.NewClient(ctx, ts)
 
 	client := github.NewClient(tc)
-	orgs, _, err := client.Activity.ListNotifications(ctx, nil)
+	listN, _, err := client.Activity.ListNotifications(ctx, nil)
 	if err != nil {
 		log.Fatalf("error: %v", err)
 	}
-	for _, o := range orgs {
-		n := NewNotification(o)
-		n.Notify()
+	for _, n := range listN {
+		nt := NewNotification(n)
+		err := nt.Notify()
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
